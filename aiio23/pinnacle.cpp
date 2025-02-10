c7da2190dd72046943a3267613a81cf084e892d1
justinlgl20 <justin_g_20@outlook.com.au>
2025-02-10 15:01:32 +0800

    fjdk

--- aiio23/pinnacle.cpp
+++ aiio23/pinnacle.cpp
@@ -28,7 +28,33 @@ int nxt() {
 
 #define pii pair<int, int>
 
+template<class T> struct SegTree { // cmb(ID,b) = b
+	const T ID{}; T cmb(T a, T b) { return max(a,b); } 
+	int n; vector<T> seg;
+	void init(int _n) { // upd, query also work if n = _n
+		for (n = 1; n < _n; ) n *= 2; 
+		seg.assign(2*n,ID); }
+	void pull(int p) { seg[p] = cmb(seg[2*p],seg[2*p+1]); }
+	void upd(int p, T val) { // set val at position p
+		seg[p += n] = val; for (p /= 2; p; p /= 2) pull(p); }
+	T query(int l, int r) {	// zero-indexed, inclusive
+		T ra = ID, rb = ID;
+		for (l += n, r += n+1; l < r; l /= 2, r /= 2) {
+			if (l&1) ra = cmb(ra,seg[l++]);
+			if (r&1) rb = cmb(seg[--r],rb);
+		}
+		return cmb(ra,rb);
+	}
+	/// int first_at_least(int lo, int val, int ind, int l, int r) { // if seg stores max across range
+	/// 	if (r < lo || val > seg[ind]) return -1;
+	/// 	if (l == r) return l;
+	/// 	int m = (l+r)/2;
+	/// 	int res = first_at_least(lo,val,2*ind,l,m); if (res != -1) return res;
+	/// 	return first_at_least(lo,val,2*ind+1,m+1,r);
+	/// }
+};
 
+SegTree<pii> seg;
 
 template<int SZ>
 struct tree {
@@ -58,7 +84,11 @@ tree<100005> r, l;
 
 int32_t main() {
 	int n,m;cin>>n>>m;vector<int>a(n);
-	for(int i=0;i<n;i++)cin>>a[i];
+	seg.init(n+5);
+	for(int i=0;i<n;i++){
+		cin>>a[i];
+		seg.upd(i, make_pair(a[i], i));
+	}
 	vector<int> psa(n,a[0]);
 	for(int i=1;i<n;i++)psa[i]=psa[i-1]+a[i];
 	function<int(int,int)>sum=[&](int l,int r){
